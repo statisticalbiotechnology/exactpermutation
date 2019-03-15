e867af0e3a8b735542e6a3c41b0a24104549562f
Lukas Kall <lukas.kall@scilifelab.se>
2019-03-15 15:29:54 +0100

    Parallellization

--- src/scatter_plots.py
+++ src/scatter_plots.py
@@ -8,43 +8,77 @@ import sys
 from meanperm import *
 from pathlib import Path
 
-def scatter_from_df(data,a_pattern,b_pattern,prefix="my_",dig_res=200,pkl_file="pvals.pkl"):
-    result_file = Path(pkl_file)
-    if not result_file.exists():
-        df = pd.DataFrame(columns=['exact', 'mwu', 'ttest'])
 
+result_df,r = pd.DataFrame(columns=['exact', 'mwu', 'ttest']),0
+def log_result(result):
+    # result_list is modified only by the main process, not the pool workers.
+    result_df.loc[r] = result
+    r += 1
+
+def p_value_calc(a,b,dig_res):
+    p1 = significance_of_mean(a,b,dig_res)[0]
+    p2 = stat.mannwhitneyu(a,b,alternative="two-sided")[1]
+    p3 = stat.ttest_ind(a, b)[1]
+    return [p1,p2,p3]
+
+# Generic Thread pool code from https://gist.github.com/heavywatal/d05a8c8a9c38ab5c895464b1d64c224f
+import multiprocessing
+import multiprocessing.pool as mpp
+def mpp_tp(call,tasks,lg_results):
+    with mpp.ThreadPool(multiprocessing.cpu_count()-1) as pool:
+        results = [pool.apply_async(call, tpl) for tpl in tasks]
+        for async_result in results:
+            try:
+                lg_result(async_result.get())
+            except ValueError as e:
+                print(e)
+
+def _core_async(data,a_pattern,b_pattern,prefix,dig_res):
         a_cols = [col for col in data.columns if a_pattern in col]
         b_cols = [col for col in data.columns if b_pattern in col]
 
+        tasks =[]
         for index, row in data.iterrows():
             a = np.array(row[a_cols],dtype='float64')
             b = np.array(row[b_cols],dtype='float64')
-            p1 = significance_of_mean(a,b,dig_res)[0]
-            p2 = stat.mannwhitneyu(a,b,alternative="two-sided")[1]
-            p3 = stat.ttest_ind(a, b)[1]
-            print(p1,p2,p3)
-            df.loc[index] = [p1,p2,p3]
+            tasks.append((a,b,dig_res,))
+        mpp_tp(p_value_calc,tasks,log_result)
 
-        df.to_pickle(result_file)
+def _core(data,a_pattern,b_pattern,prefix,dig_res):
+        a_cols = [col for col in data.columns if a_pattern in col]
+        b_cols = [col for col in data.columns if b_pattern in col]
+        for index, row in data.iterrows():
+            a = np.array(row[a_cols],dtype='float64')
+            b = np.array(row[b_cols],dtype='float64')
+            ps = p_value_calc(a,b,dig_res)
+            log_result(ps)
+            print(ps)
+
+
+def scatter_from_df(data,a_pattern,b_pattern,prefix="my_",dig_res=200,pkl_file="pvals.pkl",threaded=True):
+    result_file = Path(pkl_file)
+    if not result_file.exists():
+        if threaded:
+            _core_async(data,a_pattern,b_pattern,prefix,dig_res)
+        else:
+            _core(data,a_pattern,b_pattern,prefix,dig_res)
+        result_df.to_pickle(result_file)
     else:
-        df = pd.read_pickle(result_file)
+        result_df = pd.read_pickle(result_file)
 
     sns.set(style="white")
     sns.set_context("talk")
-    g = sns.pairplot(df,plot_kws={"s": 3})
+    g = sns.pairplot(result_df,plot_kws={"s": 3})
     sns.despine()
     g.savefig(prefix+"3meth_calibration.png")
     plt.show()
 
-    low = min(df["exact"])
-    hi = max(df["exact"])
+    low = min(result_df["exact"])
+    hi = max(result_df["exact"])
     f, ax = plt.subplots(figsize=(7, 7))
     ax.set(xscale="log", yscale="log")
-    g=sns.regplot("exact", "ttest", df, ax=ax, fit_reg=False, scatter_kws={"s": 3})
+    g=sns.regplot("exact", "ttest", result_df, ax=ax, fit_reg=False, scatter_kws={"s": 3})
     g.plot([low,hi], [low,hi], 'k-', linewidth=1)
     sns.despine()
     f.savefig(prefix+"loglog_calibration.png")
     plt.show()
-
-
-
